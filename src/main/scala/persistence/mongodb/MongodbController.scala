b19395ba478c039841381d026f6452799637eb69
Stefan <stefanleminh@gmail.com>
2018-06-25 02:21:29 +0200

    Make saving work

--- src/main/scala/persistence/mongodb/MongodbController.scala
+++ src/main/scala/persistence/mongodb/MongodbController.scala
@@ -1,25 +1,44 @@
 package persistence.mongodb
 
 import controller.Controller
-import model._
 import org.mongodb.scala._
-import org.mongodb.scala.model.CreateCollectionOptions
 import Helpers._
-import org.mongodb.scala.connection.NettyStreamFactoryFactory
+import org.mongodb.scala.bson.collection.immutable.Document
+
+import scala.collection.mutable.ListBuffer
 
 case class MongodbController(controller: Controller) {
   System.setProperty("org.mongodb.async.type", "netty")
   val mongoClient: MongoClient = MongoClient()
+
   val database: MongoDatabase = mongoClient.getDatabase("ChessCluster")
   def load(sessionid: Int): Unit = {
     print("Test")
   }
 
   def save(): Unit = {
-    val collection: MongoCollection[Document] = database.getCollection("test")
+    val p1 = controller.playerA.toString
+    val p2 = controller.playerB.toString
+    val session = Document("id" -> 0, "round" -> controller.round, "PlayerA" -> p1, "PlayerB" -> p2)
+    var field = new ListBuffer[Document]
+
+    for{
+      i <- 0 to 7
+      j <- 0 to 7
+    }yield {
+      val fig = controller.gamefield.get(i, j)
+      if(controller.playerA.hasFigure(fig)){
+        field += Document("id" -> 0, "designator" -> fig.toString(), "Position" -> Document("x"-> i, "y" -> j), "Player" -> p1)
+      }
+
+      if(controller.playerB.hasFigure(fig)){
+        field += Document("id" -> 0, "designator" -> fig.toString(), "Position" -> Document("x"-> i, "y" -> j), "Player" -> p2)
+      }
+    }
+    val collection: MongoCollection[Document] = database.getCollection("sessions")
     collection.drop().results()
-    val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
-      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
+    val doc: Document = Document("sessionid" -> 0, "p1" -> p1, "p2" -> p2,
+      "gamefield" -> field)
     collection.insertOne(doc).results()
 
     // get it (since it's the only one in there since we dropped the rest earlier on)
